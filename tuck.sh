7aa9c53d2d40563d48015e5b59314a5be9938c58
Daniel Henderson <tindrum@mac.com>
2016-03-24 14:02:02 -0700

    basic installation instructions

--- tuck.sh
+++ tuck.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
+
+#
+# User will need to `source` this file from their .bashrc or .bash_profile 
+#
+
 # echo "in shell shim"
 # someone says this will write temp history to .bash_history
 # history -a
@@ -8,7 +13,7 @@
 # history 2 | cat
 # env TUCK_HISTORY=$(history) env TUCK_LAST=$(history 2) /Users/tindrum/.local/bin/tuck "$@"
 # env TUCK_HISTORY=$(history) env TUCK_LAST=$(history 2) /Users/tindrum/Documents/Sp_2016_local/CS_462_local/TuckList/cli/tucklist/python-tucklist/tuck.py "$@"
-echo "*** function tuck loading ***"
+# echo "*** function tuck loading ***"
 function tuck {
     # Get the last 2 history commands.
     # This will generate a string like this:
