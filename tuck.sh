82979706cdd74560a67a6c2e0f7f150d9ee95327
Daniel Henderson <tindrum@mac.com>
2016-03-24 21:38:54 -0700

    installs in ~/bin/tucklist/ directory

--- tuck.sh
+++ tuck.sh
@@ -15,20 +15,16 @@
 # env TUCK_HISTORY=$(history) env TUCK_LAST=$(history 2) /Users/tindrum/Documents/Sp_2016_local/CS_462_local/TuckList/cli/tucklist/python-tucklist/tuck.py "$@"
 # echo "*** function tuck loading ***"
 function tuck () {
-    # Get the last 2 history commands.
-    # This will generate a string like this:
-    # NNN command  NNN+1 tuck
-    # Python will see a single argument, with 2 strings
-    # separated by newline
-    # It will use the first, since the 2nd will be 'tuck'
     if [ $# -gt 0 ]; then
         echo "Your command line contains $# arguments"
         echo "we need to see if the first one is -h"
         echo "and the third one could be a number or some word?"
     else
         last=`history 2`
-        /Users/tindrum/Documents/Sp_2016_local/CS_462_local/TuckList/cli/tucklist/python-tucklist/tuck.py "$last $@"
+        ~/bin/tucklist/tuck.py "$last $@"
         echo "last command tucked"
     fi
 }
-echo "*** tuck loaded ***"
+bold=$(tput bold)
+normal=$(tput sgr0)
+echo ">>> ${bold}tuck${normal} command loaded"
